4cb056fae27aa52ec810a6fd035f59492da8a1d1
Cristine Shipman <cristine.shipman@gmail.com>
2017-06-22 16:07:21 -0500

    updated today page

--- public/style.js
+++ public/style.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var bar = new ProgressBar.Circle(circle, {
   color: '#aaa',
   // This has to be the same size as the maximum width to
@@ -19,49 +18,12 @@ var bar = new ProgressBar.Circle(circle, {
 
     var value = Math.round(circle.value() * 100);
     if (value === 0) {
-      circle.setText('');
+      circle.setText("+");
     } else {
-      circle.setText(value);
+      circle.setText("+");
     }
 
   }
-=======
-$( document ).ready(function() {
-    $('.plus-button').click(function(){
-      var api_url = '/api/drinkup';
-        axios.put(api_url)
-          .then(function (response) {
-            console.log(response.data);
-            if(response.data.value >0){
-              $("#cup1").css("color", "blue");
-            }
-            if(response.data.value >1){
-              $("#cup2").css("color", "blue");
-            }
-            if(response.data.value >2){
-              $("#cup3").css("color", "blue");
-            }
-            if(response.data.value >3){
-              $("#cup4").css("color", "blue");
-            }
-            if(response.data.value >4){
-              $("#cup5").css("color", "blue");
-            }
-            if(response.data.value >5){
-              $("#cup6").css("color", "blue");
-            }
-            if(response.data.value >6){
-              $("#cup7").css("color", "blue");
-            }
-            if(response.data.value >7){
-              $("#cup8").css("color", "blue");
-            }
-          })
-          .catch(function (error) {
-            console.error(error);
-          });
-      });
->>>>>>> master
 });
 bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
 bar.text.style.fontSize = '2rem';
