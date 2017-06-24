0d2163692bae216cc27e15c259691dc234e16248
Sokhun Seng <sososeng@gmail.com>
2017-06-24 10:56:47 -0500

    fixed all the error

--- public/style.js
+++ public/style.js
@@ -1,56 +1,13 @@
 
-      //circle graph
-      try {
-        var bar = new ProgressBar.Circle(circle, {
-          color: '#aaa',
-          // This has to be the same size as the maximum width to
-          // prevent clipping
-          strokeWidth: 4,
-          trailWidth: 1,
-          easing: 'easeInOut',
-          duration: 1400,
-          text: {
-            autoStyleContainer: false
-          },
-          from: { color: '#5ce2ed', width: 5 },
-          to: { color: '#5ce2ed', width: 4 },
-          // Set default step function for all animate calls
-          step: function(state, circle) {
-            circle.path.setAttribute('stroke', state.color);
-            circle.path.setAttribute('stroke-width', state.width);
-            circle.setText("+");
-          }
-        });
-        bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-        bar.text.style.fontSize = '2rem';
-
-        bar.animate(0);  // Number from 0.0 to 1.0s
-      } catch (error) {
-        console.error(error);
-      }
 
 $(document).ready(function(){
+
   var url = location.href;
   if (url.endsWith('/today')) {
     $("#nav-today").addClass('active');
   } else if (url.endsWith('/status')) {
     $("#nav-status").addClass('active');
   }
-
-  (function(){
-    var offset = moment().utcOffset();
-    var api_url = '/api/synctime:'+offset;
-       axios.put(api_url)
-          .then(function (response) {
-            console.log(response.data);
-
-           })
-           .catch(function (error) {
-             console.error(error);
-           });
-       })();
-
-
 
 
 });
