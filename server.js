84c56d792a0473152b8675d8d422c6280b5ea4d2
Sokhun Seng <sososeng@gmail.com>
2017-06-26 23:57:32 -0500

    adding push nulificaiton

--- server.js
+++ server.js
@@ -329,12 +329,9 @@ app.put('/api/adddevice:device/:sub', isLoggedIn , function(req, res) {
     if(req.params.sub === "no"){
       User.findOneAndUpdate({ 'local._id' : req.user.local._id},{$addToSet: {"local.device": req.params.device}, $set: { "local.subscribe": false }} ,{new:true});
     }*/
-  if(req.params.sub == "yes"){
-    res.json({value:1});
-  }
-  if(req.params.sub == "no"){
-    res.json({value:0});
-  }
+
+    res.json({value:req.params.sub});
+
 });
 
 
